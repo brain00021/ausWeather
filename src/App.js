b1e7e6f4622b7d97c7dab73b9d6ebad2aa9c32fe
Brian li <brain00021@gmail.com>
2019-12-21 22:08:47 +1100

    1.Update Item  Components 2.Update Api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{ Component }from 'react';
 import './App.css';
+import WeatherData from './data.json';
+import Item from './item/Item'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // state = {
+  //   repos: []
+  // }
+  // componentDidMount() {
+  //   this.handleSearch()
+  // }
+  // handleSearch = () =>{
+  //   let url = 'http://api.openweathermap.org/data/2.5/weather?q=London';
+  //   fetch(url).then(response => response.json()).then((repos) => {
+  //     console.log(repos);
+  //     console.log(repos.length);
+  //     this.setState({
+  //       repos: this.state.repos
+  //     });
+  //   }).catch(response => {
+  //     console.log(response)
+  //   });
+  // };
+  render() {
+    return (
+      
+      <div className="App">
+        <div className="WeatherWrapper">
+        <h1>City Weather Data</h1>
+        <div className="WeatherInfoWrapper">
+          {
+            WeatherData.map((text,idx) => (
+            <Item data={text} key={idx}/>))
+          }
+        </div>
+
+        </div>
+      </div>
+    );
+  }
+
 }
-
+module.exports = {
+  WeatherData: WeatherData
+}
 export default App;
---
+++ src/item/Item.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+// import styled from 'styled-components';
+import style from './Item.module.css';
+
+let months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+class Item extends Component {
+    render() {
+      const {
+        name,
+        main,
+        dt,
+        weather
+      } = this.props.data;
+
+      const D = new Date(dt);
+      let Year = D.getFullYear(),
+      Mouth = months[D.getMonth()],  
+      Day = D.getDate(),
+      Hour = D.getHours(),
+      Mintue = D.getMinutes();  
+
+      //  picture expression
+      const PICTURE_EXPRESSION = /\.(png|jpe?g|gif|svg)(\?.*)?$/
+      const picReg = new RegExp (PICTURE_EXPRESSION)
+
+      return (
+        <div className={style.infoCard}>
+            <div className={style.timeIcon}>
+                <span>{Day}  {Mouth} {Year}</span>
+                <h2>{name}</h2>
+                <span>{(Hour < 10)? `0${Hour}`:Hour} : {(Mintue < 10)? `0${Mintue}`:Mintue}</span>
+            </div>
+            <div className={style.importWrapper}>
+              <div className={style.degreeIcon}>
+                <h4>{main.temp.toFixed(0)}°</h4>
+              </div>
+              {
+                weather.map((data,idx) => (
+                  <div key={idx} className={style.weatherIcon}>
+                    <img src={(!picReg.test(data['icon'])) ? `http://openweathermap.org/img/wn/${data['icon']}@2x.png`:data['icon']} alt={data['icon']}/> 
+                    <span>{data['main']}</span>
+                  </div>
+                ))
+              }
+            </div>
+        </div>
+      );
+    }
+  }
+  
+  export default Item;
