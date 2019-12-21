47603b1c4075efa929cab1a1bb328f746574e921
Brian li <brain00021@gmail.com>
2019-12-21 22:32:34 +1100

    Setting and Description

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+// JEST has import issue. Still try to figure out.
+// const Item = require('./item/Item');
+
+// automatically unmount and cleanup DOM after the test is finished.
+// afterEach(cleanup);
+
+const sum = require('./sum');
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
+
+const WeatherData  =  './data.json';
+test('Check Data Filename Extension/ Data has City Name',() => {
+  expect(WeatherData).toMatch(/json/i);
+  for(let i in WeatherData){
+    expect(WeatherData[i].name).not.toBe('')
+  }
+})
+
